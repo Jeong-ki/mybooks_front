bc4f6dd6de3ed9606af96ed3ff8ebeb7d9c225a9
Jeong-ki <7863z@naver.com>
2022-12-28 01:48:19 +0900

    Add: 책 상세 정보 마크업

--- src/components/BookDetail.tsx
+++ src/components/BookDetail.tsx
@@ -2,24 +2,38 @@ import moment from "moment";
 import Image from "next/image";
 import styles from "src/styles/components/BookDetail.module.css";
 import { KakaoBookInfo } from "src/types";
+import { StarImgList } from "src/constant";
 
 export default function BookDetail({ book }: { book: KakaoBookInfo }) {
-  console.log(moment(book.datetime));
+  console.log(moment(book.datetime).format("YYYY.MM.DD"));
 
   return (
-    <div>
-      <Image src={book.thumbnail} width={110} height={160} alt={""} />
-      <div>
+    <div className={styles.book_detail}>
+      <div className={styles.thumbnail_wrapper}>
+        <Image src={book.thumbnail} width={120} height={174} alt={""} />
+      </div>
+      <div className={styles.book_info}>
         <h2>{book.title}</h2>
-        <p>
-          {book.authors.join(",")} <small>지음</small> |{" "}
+        <p className={styles.author}>
+          {book.authors.join(",")} <small>지음</small> ·{" "}
           {book.translators.join(",")} <small>옮김</small>
         </p>
-        <div>* * * * * 3.45점</div>
-        <p>평가 120 · 리뷰 52</p>
-        <p>출판: {book.publisher}</p>
-        <p>출간일: {book.datetime.split("T")[0]}</p>
-        <p>ISBN: {book.isbn.split(" ")[1]}</p>
+        <div className={styles.grade}>
+          <Image src={StarImgList[2]} alt="" width={18 * 3} height={18} /> 3.45
+          <p className={styles.total}>평가 120 · 리뷰 52</p>
+        </div>
+        <div className={styles.detail_info}>
+          <p>
+            <strong>출판:</strong> {book.publisher}
+          </p>
+          <p>
+            <strong>출간일:</strong>{" "}
+            {moment(book.datetime).format("YYYY.MM.DD")}
+          </p>
+          <p>
+            <strong>ISBN:</strong> {book.isbn.split(" ")[1]}
+          </p>
+        </div>
       </div>
     </div>
   );
